03bb5a4edc97eedaa9828da8d9efd2b490f637b0
lloganat <l.murukesan.loganathan@intel.com>
2026-06-09 15:00:33 +0800

    Fix duplicate assembly attributes in AssemblyInfo.cs

--- 20vNG/Properties/AssemblyInfo.cs
+++ 20vNG/Properties/AssemblyInfo.cs
@@ -4,12 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-//[assembly: AssemblyTitle("20vNG")]
-//[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-//[assembly: AssemblyCompany("Intel Corporation")]
-//[assembly: AssemblyProduct("20vNG")]
-//[assembly: AssemblyCopyright("Copyright © Intel Corporation 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
